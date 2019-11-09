4851147d0c61795768a440dddfb4a61cb0c6675a
mplumador <mplumador@umass.edu>
2019-11-09 06:31:55 -0500

    99% of the buttons have click events and update state

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -8,21 +9,80 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
-  currCalculation="";
+  currNumbers=[];
+  currOperators=[];
+  currNumber="";
+
+  changeSign()
+  {
+    if(this.currNumber.slice(0,1) ==="-")
+    {
+      this.currNumber = this.currNumber.slice(1,this.currNumber.length);
+      console.log(this.currNumber);
+    }
+    else
+    {
+      this.currNumber = "-"+this.currNumber;
+      console.log(this.currNumber);
+    }
+
+  }
   inputOperator(op)
   {
-    this.currCalculation += op;
-    console.log(this.currCalculation);
+    //If the currentNumber is null, then we change the last operator we put in to the new one
+    if(this.currNumber === "")
+    {
+      this.currOperators[this.currOperators.length-1] = op;
+      return;
+    }
+    this.currNumbers.push(this.currNumber);
+    this.currNumber="";
+    this.currOperators.push(op);
+
+    console.log(this.currNumbers);
+    console.log(this.currOperators);
+
   }
   inputNumber(num)
   {
-    this.currCalculation += num;
-    console.log(this.currCalculation);
+    this.currNumber += num;
   }
 
   calculateResult()
   {
-    console.log(this.currCalculation+"=Fuck you");
+    //Append the currNumber in the event of a correct input
+    if(this.currNumber !== "")
+    {
+      this.currNumbers.push(this.currNumber);
+    }
+
+    //If we push calculate result before appending a second number
+    //Remove the last operator
+    if(this.currNumbers.length !== this.currOperators.length+1)
+    {
+      this.currOperators.splice(this.currOperators.length-1,this.currOperators.length);
+      console.log(this.currOperators);
+    }
+    //Display the string
+    let j =0;
+    let calculateString = "";
+    for(let i=0; i < this.currOperators.length; i++)
+    {
+      calculateString += this.currNumbers[j] +this.currOperators[i];
+      j+=1;
+    }
+    calculateString += this.currNumbers[j]
+    console.log(calculateString);
+
+    //Reset the state
+    this.resetState();
+  }
+
+  resetState()
+  {
+    this.currNumber="";
+    this.currNumbers=[];
+    this.currOperators=[];
   }
 
 }
